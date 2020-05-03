42aaa9bc02189388bbc2c08e2404704ceacfd475
SantyVill <santyvillagra@gmail.com>
2020-05-02 23:26:58 -0300

    modifico tp4.cpp (punto 6)

--- tp4.cpp
+++ tp4.cpp
@@ -9,13 +9,19 @@ typedef struct
     char *descripcion;
 } tarea;
 
-
+//=========================FUNCIONES=========================
 void cargarTareas(tarea *tareas,int n);
 int realizarTareas(tarea *tareasP, tarea *tareasR,int n);
+void mostrarTareas(tarea *tareas,int n);
+tarea* buscarTarea(tarea *tareasP,tarea *tareasR,int n,int c);
+bool buscar(char *frase,char *palabra);
+
+//===========================MIAN===========================
 
 int main(){
     srand (time(NULL));
     int n,i,opc,c=0;
+    tarea *paux;
     printf("Ingrese la cantidad de tareas a cargar: ");
     scanf("%d",&n);
     getchar();
@@ -23,29 +29,92 @@ int main(){
     tarea *tareasRealizadas=(tarea *)malloc(sizeof(tarea)*n);
     cargarTareas(tareasPendientes, n);
     c=realizarTareas(tareasPendientes,tareasRealizadas,n);
-    
-    printf("\n\nTareas realizadas: ");
-    for (i = 0; i < c; i++)
-    {
-            printf("\n\nTarea numero %d",(tareasRealizadas+i)->tareaID);
-            printf("\nDescripcion: %s",(tareasRealizadas+i)->descripcion);
-            printf("\nDuracion: %d",(tareasRealizadas+i)->duracion);
-    }
+    printf("\n\nTareas Realizadas: ");
+    mostrarTareas(tareasRealizadas,c);
     printf("\n\nTareas Pendientes: ");
-    for (i = 0; i < n; i++)
-    {
-        if ((tareasPendientes+i)->descripcion!=NULL)
-        {
-            printf("\n\nTarea numero %d",(tareasPendientes+i)->tareaID);
-            printf("\nDescripcion: %s",(tareasPendientes+i)->descripcion);
-            printf("\nDuracion: %d",(tareasPendientes+i)->duracion);
-        }
-    }
+    mostrarTareas(tareasPendientes,n);
+
+    paux=buscarTarea(tareasPendientes,tareasRealizadas,n,c);
+    printf("\nID de tarea: %d",paux->tareaID);
+    printf("\nDescripcion: %s",paux->descripcion);
+    printf("\nDuracion: %d",paux->duracion);
+    
     getchar();
     getchar();
     free (tareasRealizadas);
     free (tareasPendientes);
     return 0;
+}
+
+//========================FUNCIONES========================
+
+tarea* buscarTarea(tarea *tareasP,tarea *tareasR,int n,int c){
+    int i,j,cont,h;
+    char palabra[15];
+    tarea *aux;
+    getchar();
+    printf("\n\nIngrese la palabra con la que quiere buscar la tarea: ");
+    gets(palabra);
+    aux=tareasP;
+    for(h=0;h<n;h++){
+        if (aux->descripcion!=NULL)
+        {
+            if (buscar(aux->descripcion,palabra))
+            {
+                printf("\nTarea pendiente:");
+                return aux;
+            }
+        }
+        aux++;
+    }
+    aux=tareasR;
+    for(h=0;h<c;h++){
+        if (aux->descripcion!=NULL)
+        {
+            if (buscar(aux->descripcion,palabra))
+            {
+                printf("\nTarea realizada:");
+                return aux;
+            }
+        }
+        aux++;
+    }
+}
+
+bool buscar(char *frase,char *palabra){
+    int i,j,c=0;
+    for (i = 0; i <= strlen(frase)-strlen(palabra); i++)
+    {
+        c=0;
+        for ( j = 0; j < strlen(palabra); j++)
+        {
+            if (*(frase+j+i)!=*(palabra+j))
+            {
+                j=strlen(palabra);
+            } else {
+                c++;
+                if (c==strlen(palabra))
+                {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+void mostrarTareas(tarea *tareas,int n){
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        if ((tareas+i)->descripcion!=NULL)
+        {
+            printf("\n\nTarea numero %d",(tareas+i)->tareaID);
+            printf("\nDescripcion: %s",(tareas+i)->descripcion);
+            printf("\nDuracion: %d",(tareas+i)->duracion);
+        }
+    }
+    
 }
 
 void cargarTareas(tarea *tareas,int n){
