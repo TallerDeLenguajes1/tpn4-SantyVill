2381638028c650718d961e5684b554dcb0b0a824
SantyVill <santyvillagra@gmail.com>
2020-05-08 03:05:09 -0300

    tp05 subo archivo tp4.cpp a la nueva rama

--- tp4.cpp
+++ tp4.cpp
@@ -9,40 +9,62 @@ typedef struct
     char *descripcion;
 } tarea;
 
+typedef struct Nodo
+{
+    tarea T;
+    Nodo *sig;
+}Nodo;
+
+
 //=========================FUNCIONES=========================
-void cargarTareas(tarea **tareas,int n);
-int realizarTareas(tarea **tareasP, tarea **tareasR,int n);
-void mostrarTareas(tarea **tareas,int n);
-tarea* busquedaPorPalabra(tarea **tareasP,tarea **tareasR,int n,int c);
+Nodo* busquedaPorPalabra(Nodo *tareasP,Nodo *tareasR);
 bool buscar(char *frase,char *palabra);
-tarea* buscquedaPorId(tarea **tareasP,tarea **tareasR,int n,int c);
+Nodo* buscquedaPorId(Nodo *tareasP,Nodo *tareasR);
+void mostrarTareas(Nodo *tareas);
+void cargarTareas(Nodo **tareas,int n);
+void realizarTareas(Nodo **tareasP, Nodo **tareasR);
 //===========================MIAN===========================
 
 int main(){
     srand (time(NULL));
-    int n,i,opc,c=0;
-    tarea *paux;
+    int n,i,opc;
+    Nodo *paux;
     printf("Ingrese la cantidad de tareas a cargar: ");
     scanf("%d",&n);
     getchar();
-    tarea **tareasPendientes=(tarea **)malloc(sizeof(tarea*)*n);
-    tarea **tareasRealizadas=(tarea **)malloc(sizeof(tarea*)*n);
-    cargarTareas(tareasPendientes, n);
-    c=realizarTareas(tareasPendientes,tareasRealizadas,n);
+    Nodo *tareasPendientes=(Nodo *)malloc(sizeof(Nodo));
+    Nodo *tareasRealizadas=(Nodo *)malloc(sizeof(Nodo));
+    tareasPendientes=NULL;
+    tareasRealizadas=NULL;
+    cargarTareas(&tareasPendientes,n);
+    realizarTareas(&tareasPendientes,&tareasRealizadas);
     printf("\n\nTareas Realizadas: ");
-    mostrarTareas(tareasRealizadas,c);
+    mostrarTareas(tareasRealizadas);
     printf("\n\nTareas Pendientes: ");
-    mostrarTareas(tareasPendientes,n);
+    mostrarTareas(tareasPendientes);
 
-    paux=busquedaPorPalabra(tareasPendientes,tareasRealizadas,n,c);
-    printf("\nID de tarea: %d",paux->tareaID);
-    printf("\nDescripcion: %s",paux->descripcion);
-    printf("\nDuracion: %d",paux->duracion);
+    paux=busquedaPorPalabra(tareasPendientes,tareasRealizadas);
+    if (paux!=NULL)
+    {
+        printf("\nID de tarea: %d",paux->T.tareaID);
+        printf("\nDescripcion: %s",paux->T.descripcion);
+        printf("\nDuracion: %d",paux->T.duracion);
+    } else
+    {
+        printf("No se encontro tarea.");
+    }
 
-    paux=buscquedaPorId(tareasPendientes,tareasRealizadas,n,c);
-    printf("\nID de tarea: %d",paux->tareaID);
-    printf("\nDescripcion: %s",paux->descripcion);
-    printf("\nDuracion: %d",paux->duracion);
+    paux=buscquedaPorId(tareasPendientes,tareasRealizadas);
+    if (paux!=NULL)
+    {
+        printf("\nID de tarea: %d",paux->T.tareaID);
+        printf("\nDescripcion: %s",paux->T.descripcion);
+        printf("\nDuracion: %d",paux->T.duracion);
+    } else
+    {
+        printf("No se encontro tarea.");
+    }
+    
     
     getchar();
     getchar();
@@ -53,35 +75,34 @@ int main(){
 
 //========================FUNCIONES========================
 
-tarea* busquedaPorPalabra(tarea **tareasP,tarea **tareasR,int n,int c){
+Nodo* busquedaPorPalabra(Nodo *tareasP,Nodo *tareasR){
     int i;
     char palabra[15];
-    tarea **aux;
+    Nodo *aux;
     getchar();
     printf("\n\nIngrese la palabra con la que quiere buscar la tarea: ");
     gets(palabra);
     aux=tareasP;
-    for(i=0;i<n;i++){
-        if (aux[i]!=NULL)
-        {
-            if (buscar(aux[i]->descripcion,palabra))
+    while(aux!=NULL)
+    {
+            if (buscar(aux->T.descripcion,palabra))
             {
                 printf("\nTarea pendiente:");
-                return aux[i];
+                return aux;
             }
-        }
+        aux=aux->sig;
     }
     aux=tareasR;
-    for(i=0;i<c;i++){
-        if (aux[i]->descripcion!=NULL)
+    while(aux!=NULL)
+    {
+        if (buscar(aux->T.descripcion,palabra))
         {
-            if (buscar(aux[i]->descripcion,palabra))
-            {
-                printf("\nTarea realizada:");
-                return aux[i];
-            }
+            printf("\nTarea realizada:");
+            return aux;
         }
+        aux=aux->sig;
     }
+    return NULL;
 }
 
 bool buscar(char *frase,char *palabra){
@@ -106,64 +127,67 @@ bool buscar(char *frase,char *palabra){
     return false;
 }
 
-tarea* buscquedaPorId(tarea **tareasP,tarea **tareasR,int n,int c){
+Nodo* buscquedaPorId(Nodo *tareasP,Nodo *tareasR){
     int id,i;
+    Nodo *aux;
     printf("\nIngrese el id de la tarea que desea buscar: ");
     scanf("%d",&id);
-    for (i = 0; i < c; i++)
+    aux=tareasR;
+    while(aux!=NULL)
     {
-        if ((tareasR[i])->tareaID==id)
+        if (aux->T.tareaID==id)
         {
-            return (tareasR[i]);
+            return aux;
         }
-        
+        aux=aux->sig;
     }
-    
-    for ( i = 0; i < n; i++)
+    aux=tareasP;
+    while (aux!=NULL)
     {
-        if ((tareasP[i])->tareaID==id)
+        if (aux->T.tareaID==id)
         {
-            return (tareasP[i]);
+            return (aux);
         }
+        aux=aux->sig;
     }
 }
 
-void mostrarTareas(tarea **tareas,int n){
-    int i;
-    for (i = 0; i < n; i++)
+void mostrarTareas(Nodo *tareas){
+    while(tareas!=NULL)
     {
-        if (tareas[i]!=NULL)
-        {
-            printf("\n\nTarea numero %d",(tareas[i])->tareaID);
-            printf("\nDescripcion: %s",(tareas[i])->descripcion);
-            printf("\nDuracion: %d",(tareas[i])->duracion);
-        }
+        printf("\n\nTarea numero %d",tareas->T.tareaID);
+        printf("\nDescripcion: %s",tareas->T.descripcion);
+        printf("\nDuracion: %d",tareas->T.duracion);
+        tareas=tareas->sig;
     }
-    
 }
 
-void cargarTareas(tarea **tareas,int n){
+void cargarTareas(Nodo **tareas,int n){
     int i;
     char descr[60];
+    Nodo *nuevoNodo;
     for ( i = 0; i < n; i++)
     {
-        tareas[i]=(tarea *)malloc(sizeof(tareas));
-        (tareas[i])->tareaID=i+1;
-        (tareas[i])->duracion=rand()%91+10;
+        nuevoNodo =(Nodo *)malloc(sizeof(Nodo));
+        nuevoNodo->T.tareaID=i+1;
+        nuevoNodo->T.duracion=rand()%91+10;
         printf("\nIngrese la descripcion de la tarea %d: ",i+1);
         gets(descr);
-        (tareas[i])->descripcion=(char *)malloc(sizeof(char)*strlen(descr));
-        strcpy((tareas[i])->descripcion,descr);
+        nuevoNodo->T.descripcion=(char *)malloc(sizeof(char)*strlen(descr));
+        strcpy(nuevoNodo->T.descripcion,descr);
+        nuevoNodo->sig=*tareas;
+        *tareas=nuevoNodo;
     }
 }
 
-int realizarTareas(tarea **tareasP, tarea **tareasR,int n){
-    int opc=0,i,c=0;
-    for (i = 0; i < n; i++)
+void realizarTareas(Nodo **tareasP, Nodo **tareasR){
+    int opc=0,band=0;
+    Nodo *nuevo,*aux=*tareasP,*auxant=*tareasP,*auxborrar;
+    while (aux!=NULL)
     {
-        printf("\nSe realizara la tarea %d?",i+1);
-        printf("\nDescripcion: %s",(tareasP[i])->descripcion);
-        printf("\nDuracion: %d",(tareasP[i])->duracion);
+        printf("\nSe realizara la tarea %d?",aux->T.tareaID);
+        printf("\nDescripcion: %s",aux->T.descripcion);
+        printf("\nDuracion: %d",aux->T.duracion);
         do
         {
             printf("\nSI --> 1.\tNO --> 2.\n");
@@ -171,11 +195,29 @@ int realizarTareas(tarea **tareasP, tarea **tareasR,int n){
         } while (opc!=1 && opc!=2);
         if (opc==1)
         {
-            tareasR[c]=(tarea *) malloc(sizeof(tarea));
-            *(tareasR[c])=*(tareasP[i]);
-            tareasP[i]=NULL;
-            c++;
+            nuevo=(Nodo *) malloc(sizeof(Nodo));
+            nuevo->T=aux->T;
+            nuevo->sig=*tareasR;
+            *tareasR=nuevo;
+
+            auxborrar=aux;
+            if (band==0)
+            {
+                *tareasP=(*tareasP)->sig;
+                aux=aux->sig;
+                free(auxborrar);
+            } else
+            {
+                auxant->sig=aux->sig;
+                aux=aux->sig;
+                free(auxborrar);
+            }
+            
+        } else
+        {
+            auxant=aux;
+            aux=aux->sig;
         }
+        band=1;
     }
-    return c;
 }
